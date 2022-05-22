09339c18da4d3f106d6b82b938dc038a713a94b9
Grzdaczek <grzegorz.jnsk@gmail.com>
2022-05-22 15:24:47 +0200

    feat: Modified Kruskal's algorithm

--- labyrinth/src/main.rs
+++ labyrinth/src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 use graph::Graph;
-use rand::prelude::*;
+use rand::{seq::SliceRandom, thread_rng};
 use std::cell::Cell;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Width of the labyrinth
-    #[clap(short, long, default_value_t = 10)]
+    #[clap(short, long, default_value_t = 20)]
     width: usize,
 
     /// Height of the labyrinth
@@ -16,52 +16,79 @@ struct Args {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // fetch arguments
     let args = Args::parse();
-
     let w = args.width;
     let h = args.height;
 
-    let mut graph: Graph<Cell<usize>, Cell<bool>> = Graph::new_undirected();
+    // generate grid
+    let graph = {
+        let mut graph: Graph<Cell<usize>, Cell<bool>> = Graph::new_undirected();
 
-    for i in 0..(w * h) {
-        graph.add_node(Cell::new(i));
-    }
-
-    for y in 0..h {
-        for x in 1..w {
-            let index = y * w + x;
-            graph.set_edge(index, index - 1, Cell::new(false));
+        for i in 0..(w * h) {
+            graph.add_node(Cell::new(i));
         }
-    }
 
-    for y in 1..h {
-        for x in 0..w {
-            let index = y * w + x;
-            graph.set_edge(index, index - w, Cell::new(false));
+        for y in 0..h {
+            for x in 1..w {
+                let index = y * w + x;
+                graph.set_edge(index, index - 1, Cell::new(false));
+            }
         }
-    }
 
-    let mut rng = thread_rng();
-    graph.edges().for_each(|x| x.value().set(rng.gen()));
+        for y in 1..h {
+            for x in 0..w {
+                let index = y * w + x;
+                graph.set_edge(index, index - w, Cell::new(false));
+            }
+        }
 
+        graph
+    };
+
+    // create queue for shuffled edges
+    let edge_refs = {
+        let mut rng = thread_rng();
+        let mut edges = Vec::from_iter(graph.edges());
+        edges.shuffle(&mut rng);
+
+        edges
+    };
+
+    // modified Kruskal's algorithm
+    edge_refs.iter().for_each(|e| {
+        let a = e.a().value().get();
+        let b = e.b().value().get();
+
+        // if edge spans nodes of two different groups,
+        // merge those groups and mark the edge
+        if a != b {
+            graph
+                .nodes()
+                .filter(|n| n.value().get() == b)
+                .for_each(|n| n.value().set(a));
+            e.value().set(true);
+        }
+    });
+
+    // display result
     let ww = w * 2 + 1;
     let hh = h * 2 + 1;
-
     for y in 0..hh {
         for x in 0..ww {
             match (x, y) {
-                // border top
-                (0, _) => print!("█"),
-                // border bottom
-                (x, _) if x == ww - 1 => println!("█"),
                 // border left
-                (_, 0) => print!("█"),
+                (0, _) => print!("█"),
                 // border right
+                (x, _) if x == ww - 1 => println!("█"),
+                // border top
+                (_, 0) => print!("█"),
+                // border bottom
                 (_, y) if y == hh - 1 => print!("█"),
                 // border inner
                 (x, y) if x % 2 == 0 && y % 2 == 0 => print!("█"),
                 // nodes
-                (x, y) if x % 2 == 1 && y % 2 == 1 => print!(" "),
+                (x, y) if x % 2 == 1 && y % 2 == 1 => print!("░"),
                 // vertical edges
                 (x, y) if y % 2 == 0 => {
                     let a = (y / 2 - 1) * w + (x / 2);
@@ -69,7 +96,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     let e = graph.edge(a, b).map(|x| x.value().get());
                     match e {
                         Some(false) => print!("█"),
-                        Some(_) | None => print!(" "),
+                        Some(_) | None => print!("░"),
                     }
                 }
                 // horizontal edges
@@ -79,7 +106,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     let e = graph.edge(a, b).map(|x| x.value().get());
                     match e {
                         Some(false) => print!("█"),
-                        Some(_) | None => print!(" "),
+                        Some(_) | None => print!("░"),
                     }
                 }
                 (_, _) => print!(" "),
